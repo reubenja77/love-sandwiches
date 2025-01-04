cead4a1a4c9185140c11299c8d65e7bc07f5cfc2
Reuben Abrahams <reubenabrahams77@gmail.com>
2025-01-04 11:22:48 +0000

    Initial commit

--- run.py
+++ run.py
@@ -21,6 +21,8 @@ def get_sales_data():
     print("Example: 10,20,30,40,50,60\n")
 
     data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")
+    
+    sales_data = data_str.split(",")
+    print(sales_data)
 
 get_sales_data()
